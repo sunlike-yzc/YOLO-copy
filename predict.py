2a0638d8421f474b945d6f061dcd6889cc46933d
Wiserking <wit22003010051@163.com>
2023-04-04 10:56:11 +0800

    first

--- predict.py
+++ predict.py
@@ -3,7 +3,7 @@
 #   整合到了一个py文件中，通过指定mode进行模式的修改。
 #-----------------------------------------------------------------------#
 import time
-
+import torch
 import cv2
 import numpy as np
 from PIL import Image
